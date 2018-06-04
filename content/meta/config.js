0d30552c2af81c344cc264e26e19b4b4203b4ccc
Alexander Trost <AlexRTrost@gmail.com>
2018-06-04 19:41:57 -0400

    Personalize config file

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteTitle: "Alex Trost - Frontend Developer", // <title>
   shortSiteTitle: "Alex Trost - Frontend Developer", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteDescription: "Alex Trost is a New Haven based Frontend Developer",
   siteUrl: "https://mrtrost.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
   authorName: "Alex Trost",
-  authorTwitterAccount: "mrtrost",
+  authorTwitterAccount: "MisterTrost",
   // info
   headerTitle: "Alex Trost",
   headerSubTitle: "Frontend Developer",
@@ -21,8 +21,7 @@ module.exports = {
   manifestDisplay: "standalone",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/a-trost" },
+    { name: "twitter", url: "https://twitter.com/MisterTrost" }
   ]
 };
