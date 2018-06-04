78e8ee226708f72ca1195199b1a94d823297518e
Alexander Trost <AlexRTrost@gmail.com>
2018-06-04 11:43:45 -0400

    Initial commit

---
+++ content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "Alex Trost - Frontend Developer", // <title>
+  shortSiteTitle: "Alex Trost - Frontend Developer", // <title> ending for posts and pages
+  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteUrl: "https://mrtrost.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "Alex Trost",
+  authorTwitterAccount: "mrtrost",
+  // info
+  headerTitle: "Alex Trost",
+  headerSubTitle: "Frontend Developer",
+  // manifest.json
+  manifestName: "Alex Trost - Frontend Developer",
+  manifestShortName: "Alex Trost", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
